bf1ee303539d89a6829a20c3fbc889f9b5d19f9e
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 17:41:15 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,5 +1,13 @@
 import requests
+from bs4 import beautifulsoup
 
-response = requests.get('https://g1.globo.com')
 
-print(response.content)
+response = requests.get('https://vimeo.com/search/sort:latest?q=AULA')
+
+content = response.content
+
+site = BeautifulSoup(content, 'html.parser')
+
+vídeo = site.find('div',attrs={'class': 'iris_p_infinite__item span-1'})
+
+print(vídeo)
