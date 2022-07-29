030e1e0a91e36954f5b697f341e35dd0959959a1
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 14:05:54 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+lista_noticias = []
 
 response = requests.get('https://g1.globo.com/')
 
@@ -8,14 +11,27 @@ content = response.content
 site = BeautifulSoup(content, 'html.parser')
 
 # HTML da notícia
-noticia = site.find('div', attrs={'class': 'feed-post-body'})
+noticias = site.findAll('div', attrs={'class': 'feed-post-body'})
 
-# Título
-titulo = noticia.find('a', attrs={'class': 'feed-post-link'})
+for noticia in noticias:
+  # Título
+  titulo = noticia.find('a', attrs={'class': 'feed-post-link'})
 
-print(titulo.text)
+  # print(titulo.text)
+  # print(titulo['href']) # link da notícia
 
-# Subtítulo: div class="feed-post-body-resumo"
-subtitulo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
+  # Subtítulo: div class="feed-post-body-resumo"
+  subtitulo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
 
-print(subtitulo.text)
+  if (subtitulo):
+    # print(subtitulo.text)
+    lista_noticias.append([titulo.text, subtitulo.text, titulo['href']])
+  else:
+    lista_noticias.append([titulo.text, '', titulo['href']])
+
+
+news = pd.DataFrame(lista_noticias, columns=['Título', 'Subtítulo', 'Link'])
+
+news.to_excel('noticias.xlsx', index=False)
+
+# print(news)
