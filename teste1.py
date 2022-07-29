b22784ba5cd1f2cd3c2241179a90e887356b4aa1
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 18:37:32 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,95 +1,158 @@
-import numpy as np
+#### NOTES
+# please keep in mind that this will create a directory "videos" in the 
+# current working directory
+
+
+
 import pandas as pd
-import json
-
-from bs4 import BeautifulSoup
-from flask import session, redirect, url_for, render_template, request
-
+import numpy as np
 import requests
-from requests_oauthlib import OAuth2Session
+import praw
+import pprint
+import pytube
+from pytube import YouTube
+import urllib.request
+from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from chromedriver_py import binary_path
+import os
+import time
 
-from . import main
-from ._const import CLIENT_ID,CLIENT_SECRET,REDIRECT_URI
+#print('hello')
+filename = os.getcwd()+'\\videos'
+os.makedirs(filename, exist_ok=True)
 
-scope=['public','private','stats']
-oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI,scope=scope)
+reddit = praw.Reddit(
+     client_id="sVYvodt7Z20Wvg",
+     client_secret="KlFb6tNGGDCNLY_ZmFbNJu6Gbllrlw",
+     user_agent="script:timelapsegrowery_scraper:v1 (by /u/owling101)"
+ )
+#print(reddit.read_only)
 
-@main.route('/',methods=['GET'])
-def index():
-    print('route with GET')
-    return render_template('index.html')
+youtube = []
+vreddit = []
+vimeo = []
+gfycat = []
+imgur = [] # leaving these out for now
+for submission in reddit.subreddit('timelapsegrowery').top(time_filter='all', limit=None):
+    #print(submission.title)
+    if 'v.redd' in submission.url:
+        vreddit.append(submission.url)
+    elif 'youtu' in submission.url:
+        youtube.append(submission.url)
+    elif 'imgur' in submission.url:
+        imgur.append(submission.url)
+    elif 'vimeo' in submission.url:
+        vimeo.append(submission.url)
+    elif 'gfycat' in submission.url:
+        gfycat.append(submission.url)
 
-@main.route('/',methods=['POST'])
-def index_post():
-    print('route with POST')
-    return render_template('index.html')
+imgur.pop(3) # photo
+# print(len(youtube))
+# print(youtube)
+# print()
+# print()
+# print(len(vreddit))
+# print(vreddit)
+# print()
+# print()
+# print(len(vimeo))
+# print(vimeo)
+# print()
+# print()
+# print(len(gfycat))
+# print(gfycat)
+# print()
+# print()
+# print(len(imgur))
+# print(imgur)
 
-@main.route('/getviews',methods=['POST'])
-def getviews():
 
-    global df_ff
-    print('trying without AUTH')
+# downloading vreddit
+for idx, i in enumerate(vreddit):
+	print('vreddit_'+str(idx))
+	try:
+	    urllib.request.urlretrieve(i+'/DASH_1080.mp4?source=fallback', filename+'\\'+'vreddit_'+str(idx)+'.mp4')
+	except:
+	    driver = webdriver.Chrome(executable_path=binary_path)
+	    driver.get("https://viddit.red/")
+	    #time.sleep(2)
+	    form = driver.find_element_by_name('url')
+	    form.send_keys(i)
+	    form.send_keys(Keys.RETURN)
+	    #time.sleep(2)
+	    vid = driver.find_element_by_id('dlbutton')
+	    link = vid.get_attribute('href')
+	    driver.close()
+	    urllib.request.urlretrieve(link, filename+'\\'+'vreddit_'+str(idx)+'.mp4')
 
-    email = request.form['email']
-    pw = request.form['password']
 
-    payload2 = {'user_account[email]': email,'user_account[password]': pw}
+# downloading vimeo
+for idx, i in enumerate(vimeo):
+	print('vimeo_'+str(idx))
+	vidUrls = []
+	driver = webdriver.Chrome(executable_path=binary_path)
+	driver.get("https://vimeo-downloader.com/")
+	form = driver.find_element_by_name('url')
+	form.send_keys(i)
+	form.send_keys(Keys.RETURN)
+	vid = driver.find_element_by_xpath('//source')
+	link = vid.get_attribute('src')
+	driver.close()
+	urllib.request.urlretrieve(link, filename+'\\'+'vimeo_'+str(idx)+'.mp4')
 
-    with requests.Session() as s:
-        p = s.post('https://filmfreeway.com/login',data=payload2)
-        r = s.get('https://filmfreeway.com/submissions/my_submissions?project=&q=&per_page=500')
-        soup = BeautifulSoup(r.text,'html.parser')
 
-    df_ff = parse_ff(soup)
 
-    # SOOOO, this is gonna do the authorization request every time...
-    authorization_url, state = oauth.authorization_url('https://api.vimeo.com/oauth/authorize')
-    return redirect(authorization_url)
+# downloading gfycat
+for idx, i in enumerate(gfycat):
+	print('gfycat_'+str(idx))
+	response = requests.get(i)
+	page = response.text
+	soup = BeautifulSoup(page, 'lxml')
+	link = soup.findAll(type='video/mp4')[1].get('src')
+	urllib.request.urlretrieve(link, filename+'\\'+'gfycat_'+str(idx)+'.mp4')
 
-@main.route('/authori',methods=['GET'])
-def authori():
-    global df_ff
-    global df_v
-    print('route with authorized')
-    resp = request.url
-    resp = resp.replace('http','https')
-    token = oauth.fetch_token('https://api.vimeo.com/oauth/access_token',
-                              authorization_response=resp,
-                              client_secret=CLIENT_SECRET)
 
-    start_date = '2019-07-29'
-    end_date = '2019-08-29'
+# downloading imgur
+for idx, i in enumerate(imgur):
+	print('imgur_'+str(idx))
+	if 'i.imgur' not in i: # fairly time consuming for only 5 vids, this can be skipped w/ the continue
+	    driver = webdriver.Chrome(executable_path=binary_path)
+	    driver.get(i)
+	    #time.sleep(2)
+	    vid = driver.find_element_by_xpath('//source')
+	    vidLink = vid.get_attribute('src')
+	    driver.close()
+	    urllib.request.urlretrieve(vidLink, filename+'\\'+'imgur_'+str(idx)+'.mp4')
+	else:
+		urllib.request.urlretrieve(i, filename+'\\'+'imgur_'+str(idx)+'.mp4')
 
-    rURL = 'https://api.vimeo.com/me/videos/stats?group_by=embed_path&start_date=2018-09-01&end_date=2019-09-01&per_page=50&filter_embed_domains=filmfreeway.com&sort=plays&direction=desc&fields=url%2Cplays%2Cfinishes%2Cloads%2Cdownloads%2Cwatched&csv=https%3A%2F%2Fapi.vimeo.com%2Fme%2Fvideos%2Fstats%3Fstart_date%3D2018-09-01%26end_date%3D2019-09-01%26fields%3Durl.domain%2Cplays%2Cfinishes%2Cloads%2Cdownloads%2Cwatched.mean_percent%26group_by%3Dembed_domain%26per_page%3D15000%26sort%3Dplays&page=1'
 
-    r = oauth.get(rURL)
-    data = r.json()['data']
-    df_v = pd.read_json(json.dumps(data))
-    df_v['subid']=df_v.url.apply(lambda x: x['path'].split('/')[-1])
-    df_v = df_v.set_index('subid')
+# downloading youtube
 
-    df_ff = df_ff.set_index('subid')
-    df_ff = df_ff.join(df_v)
-    df_ff = df_ff.fillna(0).rename(columns={'loads': 'Loads', 'plays': 'Plays'})
-    #df_ff = df_ff.dropna().reset_index()
-    df_ff = df_ff[['Festival','Project','Notification Date','Loads','Plays']]
+# not great with pytube, and using selenium is annoying because of 
+# ad popups on most sites, would recommend manually downloading failed yt vids
+# http://ytmp3.cc/ is a good place
 
-    return render_template('views.html',
-                           output = [df_ff.to_html(classes=["table-bordered", "table-striped", "table-hover"],
-                                                   header=True,float_format=lambda x: '%d' %x,
-                                                   index=False)])
-
-def parse_ff(soup):
-    rows = soup.find_all("div", class_="tableRow")
-    subid = []
-    notifdate = []
-    festival = []
-    project = []
-
-    for row in rows:
-        subid.append(row.get('data-submission'))
-        notifdate.append(row.find_all('div',class_='DateDesktop')[0].get_text().strip())
-        festival.append(row.find_all('div',class_='Festival')[0].get_text().strip())
-        project.append(row.find_all('div',class_='Project')[0].get_text().strip())
-
-    return pd.DataFrame(np.array([subid,notifdate,festival,project]).T,columns=['subid','Notification Date','Festival','Project'])
+fails = []
+for idx, i in enumerate(youtube):
+	print('youtube'+str(idx))
+	splitI = i.split('/')
+	newLink = splitI[0]+'//'+'www.youtube.com/watch?v='+splitI[3]
+	try:
+	    streams = YouTube(newLink).streams.filter(only_video=True)
+	except Exception as e:
+	    print(e)
+	    streams = []
+	    fails.append(i)
+	for j in streams:
+	    try:
+	        j.download(os.getcwd()+'\\vids')
+	        break
+	    except Exception as f:
+	        print(f)
+	        continue 
+print(fails)
