0bac576484c87fc7296566afb649234f2dde5658
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 17:20:09 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -2,10 +2,4 @@ import requests
 
 response = requests.get('https://vimeo.com/search/sort:latest?q=AULA')
 
-print('Status code:', response.status_code)
-
-print('↓↓Header↓↓')
-print(response.headers)
-
-print('\n↓↓Content↓↓')
 print(response.content)
