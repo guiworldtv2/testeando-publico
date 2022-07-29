58735069c5e8a0be2a00124776fd7491cb6a8b2f
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 13:59:28 -0300

    Create teste1.py

---
+++ teste1.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://g1.globo.com/')
+
+content = response.content
+
+site = BeautifulSoup(content, 'html.parser')
+
+# HTML da notícia
+noticias = site.findALL('div', attrs={'class': 'feed-post-body'})
+
+print (noticias)
+
+# # Título
+titulo = noticia.find('a', attrs={'class': 'feed-post-link'})
+
+print(titulo.text)
+
+# Subtítulo: div class="feed-post-body-resumo"
+subtitulo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
+
+print(subtitulo.text)
