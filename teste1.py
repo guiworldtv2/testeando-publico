73eedd9791fb4a0e08dd60786541512865ca9fe4
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 18:48:23 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,66 +1,38 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
-"""
- Wikipedia (Web
-"""
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-from urllib.parse import urlencode
-from json import loads
-from dateutil import parser
+lista_noticias = []
 
-# about
-about = {
-    "website": 'https://vimeo.com/',
-    "wikidata_id": 'Q156376',
-    "official_api_documentation": 'http://developer.vimeo.com/api',
-    "use_official_api": False,
-    "require_api_key": False,
-    "results": 'HTML',
-}
+response = requests.get('https://archive.org/details/movies?query=tv&and[]=mediatype%3A%22movies%22')
 
-# engine dependent config
-categories = ['videos']
-paging = True
+content = response.content
 
-# search-url
-base_url = 'https://vimeo.com/'
-search_url = base_url + '/search/page:{pageno}?{query}'
+site = BeautifulSoup(content, 'html.parser')
 
+# HTML da notícia
+noticias = site.findAll('div', attrs={'class': 'item-ia hov'})
 
-# do search-request
-def request(query, params):
-    params['url'] = search_url.format(pageno=params['pageno'], query=urlencode({'q': query}))
+for noticia in noticias:
+  # Título
+  titulo = noticia.find('a', attrs={'class': 'ttl'})
 
-    return params
+  # print(titulo.text)
+  # print(titulo['href']) # link da notícia
+
+  # Subtítulo: div class="feed-post-body-resumo"
+  subtitulo = noticia.find('div', attrs={'class': 'by C C4'})
+
+  if (subtitulo):
+    # print(subtitulo.text)
+    lista_noticias.append([titulo.text, subtitulo.text, titulo['href']])
+  else:
+    lista_noticias.append([titulo.text, '', titulo['href']])
 
 
-# get response from search-request
-def response(resp):
-    results = []
-    data_start_pos = resp.text.find('{"filtered"')
-    data_end_pos = resp.text.find(';\n', data_start_pos + 1)
-    data = loads(resp.text[data_start_pos:data_end_pos])
+news = pd.DataFrame(lista_noticias, columns=['Título', 'Subtítulo', 'Link'])
 
-    # parse results
-    for result in data['filtered']['data']:
-        result = result[result['type']]
-        videoid = result['uri'].split('/')[-1]
-        url = base_url + videoid
-        title = result['name']
-        thumbnail = result['pictures']['sizes'][-1]['link']
-        publishedDate = parser.parse(result['created_time'])
+news.to_excel('noticias.xlsx', index=False)
 
-        # append result
-        results.append(
-            {
-                'url': url,
-                'title': title,
-                'content': '',
-                'template': 'videos.html',
-                'publishedDate': publishedDate,
-                'iframe_src': "https://player.vimeo.com/video/" + videoid,
-                'thumbnail': thumbnail,
-            }
-        )
+# print(news)
 
-    # return results
-    return results
