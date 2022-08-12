d9ad2227a2be5c5303b0114126fe2d3b813572d5
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-12 05:18:03 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,26 +1,81 @@
+from inspect import Traceback
+import docx2txt #para arquivos docx
+import PyPDF2 #para pdf
 import os
-import bs4
-import requests
-import pandas as pd
+import PySimpleGUI as sg
+from funcoes import speak, get_audio, janela_inicial, janela_speech_recognition
+from docx import Document
 
-PATH = os.path.join("C:\\","Users","frevert","Documents","py")
 
-def table_to_df(table):
-	return pd.DataFrame([[td.text for td in row.findAll('td')] for row in table.tbody.findAll('tr')])
-
-def next_page(soup):
-	return "http:" + soup.find('a', attrs={'rel':'next'}).get('href')
-
-res = pd.DataFrame()
-url = "http://bank-code.net/country/FRANCE-%28FR%29/"
-counter = 0
+#janelas iniciais
+janela1, janela2 = janela_inicial(), None
 
 while True:
-	print(counter)
-	page = requests.get(url)
-	soup = bs4.BeautifulSoup(page.content, 'lxml')
-	table = soup.find(name='table', attrs={'id':'tableID'})
-	res = res.append(table_to_df(table))
-	res.to_csv(os.path.join(PATH,"BIC","table.csv"), index=None, sep=';', encoding='iso-8859-1')
-	url = next_page(soup)
-	counter += 1
+    window, event, values = sg.read_all_windows()
+
+    if window == janela1 and event == 'Exit' or event == sg.WIN_CLOSED:
+        break
+
+    if window == janela1 and event == 'ler_arquivo':
+        filepath = values['-FILE-']
+        nome_arquivo = os.path.basename(filepath)
+        window['arquivo_selecionado'].update(f'Arquivo lido: {nome_arquivo}', text_color = 'green')
+
+        try:
+            if nome_arquivo.split(".")[-1] == 'txt':
+                with open(filepath) as text_to_read:
+                    txt = text_to_read.read()
+                    speak(txt)
+            elif nome_arquivo.split(".")[-1] == 'docx':
+                docx_text = docx2txt.process(filepath)
+                speak(docx_text)
+            elif nome_arquivo.split(".")[-1] == 'pdf':
+                # creating a pdf file object 
+                pdfFileObj = open(filepath, 'rb') 
+                # creating a pdf reader object 
+                pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+                    
+                # creating a page object 
+                pageObj = pdfReader.getPage(0) 
+                    
+                # extracting text from page 
+                pdf_text = pageObj.extractText() 
+                    
+                # closing the pdf file object 
+                speak(pdf_text)
+                pdfFileObj.close() 
+            else:
+                window['arquivo_selecionado'].update('Arquivo não suportado!',text_color = 'red')    
+        except:
+            window['arquivo_selecionado'].update('Arquivo vazio!',text_color = 'red')
+            
+
+
+        if filepath == '':
+            window['arquivo_selecionado'].update('Por favor selecione um arquivo!',text_color = 'red')
+    
+    if window == janela1 and event == 'ditar_texto':
+        janela2 = janela_speech_recognition()
+        janela1.hide()
+
+    if window == janela2 and event == 'back':
+        janela2.hide()
+        janela1.un_hide()
+    try:
+        if window == janela2 and event =='txt_button':
+            window['texto_informativo'].update('Arquivo criado com sucesso!', text_color = 'green')
+            with open('arquivo.txt', 'w', encoding='utf-8') as text_file:
+                said = get_audio()
+                text_file.write(said)
+                
+        if window == janela2 and event =='docx_button':
+            window['texto_informativo'].update('Arquivo criado com sucesso!', text_color = 'green')
+            #instancia docx
+            documento = Document()
+            said_docx = get_audio()
+            documento.add_paragraph(said_docx)
+            documento.save('demo.docx')
+    except:
+        window['texto_informativo'].update('Ocorreu algum erro, por favor tente novamente!', text_color = 'red')        
+
+window.close()
